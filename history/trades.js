5dd569a6896cebb99dc7b781f8c88972e70ced56
anguslogan22 <angus.logan.2005@gmail.com>
2025-07-07 22:23:22 +0100

    Update trades.js

--- history/trades.js
+++ history/trades.js
@@ -1,18 +1,226 @@
 window.tradeHistory = [
+    {
+    ticker: 'TSLA',
+    date: '2025-07-01',
+    industry: 'Automotive',
+    profit: -0.5310,
+    rationale: 'Recently listened to a great podcast by Cathie Wood and started to see the vision more and more with Tesla. A company which I think will be well beyond $300 in the future, revolutionising the world of AI and autonomous veichles.',
+    lessons: 'Unfortunatley caught a bad wave on this one, was meant to be a long term hold and used too much leverage and got caught out in Trump politics. Note to self: Dont use too much leverage when doing long term investments.'
+  },
   {
-    ticker: 'AAPL',
-    date: '2024-11-02',
-    industry: 'Technology',
-    profit: 0.35,
-    rationale: 'I believed Apple would surprise with strong iPhone demand so I bought before earnings.',
-    lessons: 'Holding through volatility paid off and reinforced patience.'
+    ticker: 'DOCU',
+    date: '2025-06-30',
+    industry: 'Software',
+    profit: 0.0610,
+    rationale: 'What can only be described as an overreaction within the market. In spite of beating both EPS (10%) AND EARNINGS (2%) company headwinds noted on conference call caused the stock to tank 40% in a day. I saw a 40% discount on a useful and still important stock.',
+    lessons: 'This current market loves to overreact, although research is crucial to make sure it isnt just fundamental issue but rather a overreaction. '
+  },
+  {
+    ticker: 'PINS',
+    date: '2025-06-30',
+    industry: 'Social Media',
+    profit: 0.0045,
+    rationale: 'A very short term trade, heard some hype on Twitter about the introduction of AI as well as a short squeeze. Managed to get out with some tiny profits.',
+    lessons: 'Dont listen to people on Twitter, by the time it is there, it is too late.'
+  },
+  {
+    ticker: 'BIDU',
+    date: '2025-06-25',
+    industry: 'Internet Services',
+    profit: 0.1520,
+    rationale: 'Another market overeaction with the Trump/China politics. I saw a great undervalued stock with upside potential going into a lower tariff region.',
+    lessons: 'In the long term, the Trump tariff news can be profitable if read correctly.'
+  },
+  {
+    ticker: 'USDCHF',
+    date: '2025-06-24',
+    industry: 'Foreign Exchange',
+    profit: -0.5017,
+    rationale: 'I was looking to get more into Forex and saw the opportunity with the Iran-Isreal conflict as a way to diversify my earnings. Using the new 50x leverage unlocked with currency on a stable currency like the francs aimed to shield diversification amid tension.',
+    lessons: 'This tension was short lived... More importantly, the leverage was way to heavy, only being down 1% and the stop loss triggering showed me there is money to be made (and lost) within forex. Definitely looking to learn more soon.'
   },
   {
     ticker: 'TSLA',
-    date: '2024-10-15',
+    date: '2025-06-24',
     industry: 'Automotive',
-    profit: -0.15,
-    rationale: 'Entered on a breakout setup but ignored overbought signals.',
-    lessons: 'Learned to respect technical exhaustion and set tighter stops.'
+    profit: 0.1134,
+    rationale: 'With the incoming release of the Autonomous Taxi in Dallas, it only made sense to join the hype train going into the 23rd of June.',
+    lessons: 'This paid off with a nice return, however, it is crucial to remember that these hypes are short lived as the stock plummeted back to its orginal price only TWO DAYS later.'
+  },
+  {
+    ticker: 'GOLD',
+    date: '2025-06-24',
+    industry: 'Precious Metals',
+    profit: -0.5052,
+    rationale: 'With the rising tensions in Iran-Isreal, I was looking to diversify some of the portfolio whilst rying out the new 25x leverage on commodities. As it looked Trump was going to get more and more involved, this was a good idea to get in as prices go up amid war scares.',
+    lessons: 'With no additoinal retaliation from the US and Iran quietning down, the gold market began to correct itself and the all time highs it had been making. Another harsh reality of 25x leverage. '
+  },
+  {
+    ticker: 'ENPH',
+    date: '2025-06-23',
+    industry: 'Solar Energy',
+    profit: -0.1508,
+    rationale: 'Looking into the correction of Bidens bill on solar energy, Trump caused a 45% correction in solar stocks. This seemed like a discount too good to be true so I bought.',
+    lessons: 'Almost being one of my best trades ever. This one hurt, a few days later after the announcement, it turned out to be halse and now the stock is back to normal levels. Without a stop loss, this would have been an incredibly profitable trade. '
+  },
+  {
+    ticker: 'SJM',
+    date: '2025-06-23',
+    industry: 'Packaged Foods',
+    profit: 0.0366,
+    rationale: 'An overreaction from decent earnings on a solid company, looking to make a little bit on the bounce back. ',
+    lessons: 'Good idea, profit is profit but not going to see the same volitality in a food company as a technology one. '
+  },
+  {
+    ticker: 'SE',
+    date: '2025-06-17',
+    industry: 'E-commerce',
+    profit: 0.1290,
+    rationale: 'Again, a large overcorrection in the market after CEO annouced "headwinds" which for one of Singapores biggest companies means nothing. Likely backed by the government, this overreaction was a huge profit opportunity.',
+    lessons: 'Always a good strategy, a good solid company which has a market overreaction, great discounted stock!'
+  },
+  {
+    ticker: 'LULU',
+    date: '2025-06-13',
+    industry: 'Apparel',
+    profit: -0.5008,
+    rationale: 'An overreaction again with one of the largest clothing companies in North America. ',
+    lessons: 'Catching a falling knife with this one. That has been two eanrings which LULU has screwed me over. I want to love the Canadian company but they are making it hard. I dont see quite where they can go from here. '
+  },
+  {
+    ticker: 'TSLA',
+    date: '2025-06-13',
+    industry: 'Automotive',
+    profit: 0.4010,
+    rationale: '',
+    lessons: ''
+  },
+  {
+    ticker: 'AMD',
+    date: '2025-06-13',
+    industry: 'Semiconductors',
+    profit: 0.1087,
+    rationale: '',
+    lessons: ''
+  },
+  {
+    ticker: 'EL',
+    date: '2025-06-13',
+    industry: 'Cosmetics',
+    profit: 0.0443,
+    rationale: '',
+    lessons: ''
+  },
+  {
+    ticker: 'EL',
+    date: '2025-06-13',
+    industry: 'Cosmetics',
+    profit: 0.1993,
+    rationale: '',
+    lessons: ''
+  },
+  {
+    ticker: 'OIL',
+    date: '2025-06-13',
+    industry: 'Energy Commodities',
+    profit: -0.0923,
+    rationale: '',
+    lessons: ''
+  },
+  {
+    ticker: 'GTLB',
+    date: '2025-06-11',
+    industry: 'Software',
+    profit: 0.1690,
+    rationale: '',
+    lessons: ''
+  },
+  {
+    ticker: 'MANU',
+    date: '2025-06-06',
+    industry: 'Sports & Entertainment',
+    profit: 0.1905,
+    rationale: '',
+    lessons: ''
+  },
+  {
+    ticker: 'CELH',
+    date: '2025-06-06',
+    industry: 'Beverages',
+    profit: 0.4184,
+    rationale: '',
+    lessons: ''
+  },
+  {
+    ticker: 'CELH',
+    date: '2025-06-06',
+    industry: 'Beverages',
+    profit: 0.3238,
+    rationale: '',
+    lessons: ''
+  },
+  {
+    ticker: 'GDX',
+    date: '2025-05-27',
+    industry: 'ETF – Gold Miners',
+    profit: 0.2795,
+    rationale: '',
+    lessons: ''
+  },
+  {
+    ticker: 'VXX',
+    date: '2025-05-23',
+    industry: 'ETN – Volatility',
+    profit: 0.0677,
+    rationale: '',
+    lessons: ''
+  },
+  {
+    ticker: 'AMD',
+    date: '2025-05-14',
+    industry: 'Semiconductors',
+    profit: 0.0853,
+    rationale: '',
+    lessons: ''
+  },
+  {
+    ticker: 'PLTR',
+    date: '2025-05-13',
+    industry: 'Software',
+    profit: 0.0270,
+    rationale: '',
+    lessons: ''
+  },
+  {
+    ticker: 'HIMS',
+    date: '2025-05-12',
+    industry: 'Telehealth',
+    profit: 0.2670,
+    rationale: '',
+    lessons: ''
+  },
+  {
+    ticker: 'HMC',
+    date: '2025-03-06',
+    industry: 'Automotive',
+    profit: 0.0500,
+    rationale: '',
+    lessons: ''
+  },
+  {
+    ticker: 'TXRH',
+    date: '2025-03-04',
+    industry: 'Restaurants',
+    profit: 0.3634,
+    rationale: '',
+    lessons: ''
+  },
+  {
+    ticker: 'CAKE',
+    date: '2025-03-04',
+    industry: 'Restaurants',
+    profit: -0.5005,
+    rationale: '',
+    lessons: ''
   }
 ];
